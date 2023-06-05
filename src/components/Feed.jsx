0fe58b4ff0e13bf50b31e84f98f4cc84d5d7eb55
JericFederer <pianocoderodyssey@gmail.com>
2023-06-05 22:15:46 +0900

    Started constructing components

---
+++ src/components/Feed.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import Sidebar from './Sidebar';
+
+
+
+const Feed = () => {
+  return (
+    <Stack
+      sx={{ 
+        flexDirection: { sx: "column", md: "row" } 
+      }}
+    >
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 }
+        }}
+      >
+        <Sidebar />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright 2023 Piano Coder Odyssey
+        </Typography>
+      </Box>
+
+      <Box>
+        <Typography>
+          <span style={{ color: "#F31503" }}>
+            Videos
+          </span>
+        </Typography>
+      </Box>
+    </Stack>
+  )
+}
+
+export default Feed
