2722a0f78132511aeb346967ea30233526839ca4
JericFederer <pianocoderodyssey@gmail.com>
2023-06-08 09:06:35 +0900

    Displaying videos

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,18 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
-import Sidebar from './Sidebar';
 
-
+import { Sidebar, Videos } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items))
+  }, [selectedCategory])
+    
   return (
     <Stack
       sx={{ 
@@ -18,7 +26,10 @@ const Feed = () => {
           px: { sx: 0, md: 2 }
         }}
       >
-        <Sidebar />
+        <Sidebar 
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
         <Typography
           className="copyright"
           variant="body2"
@@ -28,12 +39,20 @@ const Feed = () => {
         </Typography>
       </Box>
 
-      <Box>
-        <Typography>
-          <span style={{ color: "#F31503" }}>
-            Videos
-          </span>
+      <Box
+        p={2}
+        sx={{ overflow: "auto", height: "90vh", flex: 2 }}
+      >
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          {selectedCategory} <span style={{ color: "#F31503" }}> Videos </span>
         </Typography>
+
+        <Videos videos={videos} />
       </Box>
     </Stack>
   )
